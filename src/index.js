e5563c5a0cb43189afdf91b230fb6bf79ef46389
woonji.kim <woonji.kim@worksmobile.com>
2019-04-06 21:41:18 +0900

    devtools update and add comment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import modules from './modules';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-const store = createStore(modules, window.devToolsExtension && window.devToolsExtension());
+const store = createStore(modules, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
     <Provider store={store}>
@@ -21,4 +21,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
